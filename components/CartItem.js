75a42343d6a9d9a0154b4e1780a1d13381a8a3d3
Saksham Bhardwaj <125785732+i-am-saksham@users.noreply.github.com>
2024-09-25 13:25:12 +0530

    Update CartItem.js

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,50 +1,8 @@
-// import toast from "react-hot-toast";
-// import { remove } from "../redux/Slices/cartSlice";
-// import useDispatch from "react";
-// import { FcDeleteDatabase } from "react-icons/fc";
-// // yhaa pe hme vohi data chahiye jo cart mai aaega isiliye humne cart.js mai props pass kiye the.
-// const CartItem = ({item, itemIndex}) => {
-
-//   const dispatch = useDispatch();
-
-//   const removeFromCart = () => {
-//     dispatch(remove(item.id));
-//     toast.success("Item Removed");
-//   }
-
-//   return (
-//     <div>
-//       <div>
-
-//         <div>
-//           <img src={item.image}/>
-//         </div>
-
-//         <div>
-//           <h1>{item.title}</h1>
-//           <h1>{item.description}</h1>
-//           <div>
-//               <p>{item.price}</p>
-//               <div onClick={removeFromCart}>
-//                 <FcDeleteDatabase/>
-//               </div>
-//           </div>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default CartItem;
-
-
 import { FcDeleteDatabase } from "react-icons/fc"
 import { useDispatch } from "react-redux";
 import { remove } from "../redux/Slices/cartSlice";
 import { toast } from "react-hot-toast";
 
-// yhaa pe hme vohi data chahiye jo cart mai aaega isiliye humne cart.js mai props pass kiye the.
 const CartItem = ({ item, itemIndex }) => {
   const dispatch = useDispatch();
 
